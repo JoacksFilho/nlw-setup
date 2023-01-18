5013826271b7addc8c01b58e07005007fcde5250
Joacks12 <joackslemos@hotmail.com>
2023-01-18 20:45:44 -0300

    Criação Aqruivo JS

---
+++ script.js
@@ -0,0 +1,13 @@
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+
+const data = {
+  run: ["01-02", "01-04", "01-06"],
+  water: ["01-01", "01-02", "01-03"],
+  food: ["01-01", "01-02", "01-03"],
+  Workout: ["01-01", "01-03", "01-05"],
+  takePills: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06"],
+}
+
+nlwSetup.setData(data)
+nlwSetup.load()
