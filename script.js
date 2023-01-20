489b3c0e3f22b1b7d77ec7eaa164a7ee147bae1e
joacksFilhoAgger <joacksfilho@agger.com.br>
2023-01-20 14:52:58 -0300

    updates script

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
 
-const data = {
-  run: ["01-02", "01-04", "01-06"],
-  water: [
-    "01-01",
-    "01-02",
-    "01-03",
-    "01-04",
-    "01-05",
-    "01-06",
-    "01-07",
-    "01-08",
-  ],
-  food: ["01-01", "01-02", "01-03"],
-  Workout: ["01-01", "01-03", "01-05"],
-  takePills: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06"],
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso")
+    return
+  }
+
+  nlwSetup.addDay(today)
 }
 
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+// const data = {
+//   run: ["01-02", "01-04", "01-06"],
+//   water: [
+//     "01-01",
+//     "01-02",
+//     "01-03",
+//     "01-04",
+//     "01-05",
+//     "01-06",
+//     "01-07",
+//     "01-08",
+//   ],
+//   food: ["01-01", "01-02", "01-03"],
+//   Workout: ["01-01", "01-03", "01-05"],
+//   takePills: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06"],
+// }
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
